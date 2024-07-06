af3eb7928f64e10198c990a9799aaf2eafbffcac
R0bbyt3 <130287531+R0bbyt3@users.noreply.github.com>
2024-07-06 16:35:51 -0300

    omg cred

--- back-end/firebase_utils.py
+++ back-end/firebase_utils.py
@@ -5,12 +5,22 @@ from utils import convert_to_float, strip_whitespace
 import os
 
 # Inicializar o Firebase
-firebase_cred = os.getenv('FIREBASE_CREDENTIALS_JSON')
-if firebase_cred:
-    cred = credentials.Certificate(firebase_cred)
-    firebase_admin.initialize_app(cred)
-else:
-    raise ValueError("Firebase credentials not found in environment variables")
+firebase_config = {
+    "type": os.getenv('FIREBASE_TYPE'),
+    "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_CERT_URL'),
+    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_CERT_URL'),
+    "universe_domain": os.getenv('FIREBASE_UNIVERSE_DOMAIN')
+}
+
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
